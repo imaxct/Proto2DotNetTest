920d7b67a8c81fbad0bf49557513a1d8a52f8653
Hao Dong <imaxct@users.noreply.github.com>
2024-08-03 01:19:53 +0000

    test

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Exchange.Data;
+
+Console.WriteLine("Hello, World!");
+var d = new DirectoryIdentityData() {
+ DatabaseGuid = Guid.NewGuid().ToString()
+};
+Console.WriteLine(d.ToString());
